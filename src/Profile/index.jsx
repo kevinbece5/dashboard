fdc4bfe8f25a47f285ef3a0d0d74dc88de13db3a
kevinbece5 <kevinbece5@gmail.com>
2019-06-13 10:24:17 -0700

    refactored Profile component

--- src/Profile/index.jsx
+++ src/Profile/index.jsx
@@ -1,13 +1,91 @@
 import React from 'react';
+import axios from 'axios';
 import * as Styles from './style';
 
 
-const Profile = (props) => (
-    <Styles.Container>
-        <Styles.ProfilePicture src={props.image} />
-        <Styles.Name>{props.name}</Styles.Name>
-        <Styles.Description>{props.description}</Styles.Description>
-    </Styles.Container>
-)
+export default class Profile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {},
+            imageEdit: false,
+            nameEdit: false,
+            descriptionEdit: false,
+            image: '',
+            name: '',
+            description: '',
+        }
+    }
 
-export default Profile;
+    componentDidMount() {
+        this.getUserInfo();
+    }
+
+    updateField = (field) => {
+        axios.post('/updateUserInfo', { id: this.state.id, field: { field, value: this.state[field] } })
+            .then(() => this.setState({ [`${field}Edit`]: false }))
+            .then(() => this.getUserInfo())
+    }
+
+    getUserInfo = () => {
+        axios.get('/getUserInfo')
+            .then((res) => {
+                const { name, description, image, id } = res.data;
+                this.setState({
+                    user: {
+                        name,
+                        description,
+                        image,
+                        id
+                    },
+                    image,
+                    name,
+                    description
+                })
+            })
+    }
+
+    name = (e) => this.setState({ name: e.target.value })
+
+    description = (e) => this.setState({ description: e.target.value })
+
+    render() {
+        return (
+            <Styles.Container>
+                {this.state.imageEdit ?
+                    <Styles.ItemContainer>
+                        <Styles.Input className={"input-group-text"} />
+                        <Styles.Edit>Save</Styles.Edit>
+                        <Styles.Edit>Back</Styles.Edit>
+                    </Styles.ItemContainer>
+                    :
+                    <Styles.ProfilePicture src={this.state.user.image} />
+                }
+                {this.state.nameEdit ?
+                    <Styles.ItemContainer>
+                        <Styles.Input value={this.state.name} onChange={this.name} className={"input-group-text"} />
+                        <Styles.Edit onClick={() => this.updateField('name')}>Save</Styles.Edit>
+                        <Styles.Edit onClick={() => this.setState({ nameEdit: false })}>Back</Styles.Edit>
+                    </Styles.ItemContainer>
+                    :
+                    <Styles.ItemContainer>
+                        <Styles.Name>{this.state.user.name}</Styles.Name>
+                        <Styles.Edit onClick={() => this.setState({ nameEdit: true })}>Edit</Styles.Edit>
+                    </Styles.ItemContainer>
+                }
+                {this.state.descriptionEdit ?
+                    <Styles.ItemContainer>
+                        <Styles.TextArea class="form-control" value={this.state.description} onChange={this.description} className={"input-group-text"} />
+                        <Styles.Edit onClick={() => this.updateField('description')}>Save</Styles.Edit>
+                        <Styles.Edit onClick={() => this.setState({ descriptionEdit: false })}>Back</Styles.Edit>
+                    </Styles.ItemContainer>
+                    :
+                    <Styles.ItemContainer>
+                        <Styles.Description>{this.state.user.description}</Styles.Description>
+                        <Styles.Edit onClick={() => this.setState({ descriptionEdit: true })}>Edit</Styles.Edit>
+                    </Styles.ItemContainer>
+                }
+            </Styles.Container>
+        )
+    }
+}
--- src/Profile/style.js
+++ src/Profile/style.js
@@ -13,9 +13,9 @@ export const Name = styled.span`
 `;
 
 export const Description = styled.span`
-    font-size: 20px;
+    margin: auto;
     padding: 10px 0;
-    align-self: center;
+    font-size: 25px;
 `;
 
 export const Container = styled.div`
@@ -23,3 +23,27 @@ export const Container = styled.div`
     justify-content: center;
     flex-direction: column;
 `;
+
+export const Edit = styled.span`
+    padding: 0 15px;
+    font-size: 15px;
+    margin: auto 0;
+`;
+
+export const ItemContainer = styled.div`
+    display: flex;
+    padding: 30px 0px;
+
+`;
+
+export const TextArea = styled.textarea`
+    &:focus {
+        outline: none;
+    }
+`
+
+export const Input = styled.input`
+    &:focus {
+        outline: none;
+    }
+`
